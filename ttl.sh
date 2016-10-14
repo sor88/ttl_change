5c2e35c2b4f1be635fb82a69be2761a7dc015267
Rak Alexander <plus@omsklug.com>
2016-10-14 16:16:39 +0500

    Скрипт GUI для быстрого изменения значения TTL

---
+++ ttl.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+inst="Установить_TTL_вручную"
+ans=$(zenity --list --text "Включение режима Yota через точку доступа с ttl=64?" --radiolist  --column "Номер" --column "Режим" TRUE Включить FALSE Выключить FALSE "$inst" );
+echo $ans
+case $ans in
+	Включить)
+		(iptables -t mangle -A PREROUTING -o eth0 -j TTL --ttl-set 65) | zenity --progress --pulsate --title="Установка TTL в iptables" --text="Прогресс"
+		zenity --info --text "Значение TTL установлено в 65"
+	;;
+
+	Выключить)
+		(iptables -t mangle -A PREROUTING -o eth0 -j TTL --ttl-set 64) | zenity --progress --pulsate --title="Установка TTL в iptables" --text="Прогресс"
+		zenity --info --text "Значение TTL установлено в $ans"
+	;;
+
+	$inst)
+		ans=$(zenity --entry --text="Введите значение TTL вручную:");
+sleep 3
+#		(iptables -t mangle -A PREROUTING -o eth0 -j TTL --ttl-set $ans) | zenity --progress --pulsate --title="Установка TTL в iptables" --text="Прогресс"
+		zenity --info --text "Значение TTL установлено в $ans"
+	;;
+
+	*)
+	exit
+	;;
+	esac
+
+echo " Значение TTL = $ans "
+#iptables -t mangle -A PREROUTING -o eth0 -j TTL --ttl-set $ans
+exit
+
