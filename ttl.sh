1d9deb797f87b3208c516b5bab9fde7a9067bb68
Rak Alexander <plus@omsklug.com>
2016-10-17 08:50:12 +0500

    Добавлены скрипты и ключи реестра для Windows систем

--- ttl.sh
+++ ttl.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
+
+#gksudo --sudo-mode
 inst="Установить_TTL_вручную"
 ans=$(zenity --list --text "Включение режима Yota через точку доступа с ttl=64?" --radiolist  --column "Номер" --column "Режим" TRUE Включить FALSE Выключить FALSE "$inst" );
+#ttlrem=$(sudo iptables -t mangle -L --line-numbers | grep TTL | cut -c 1-2)
 echo $ans
 case $ans in
 	Включить)
-		(iptables -t mangle -A PREROUTING -o eth0 -j TTL --ttl-set 65) | zenity --progress --pulsate --title="Установка TTL в iptables" --text="Прогресс"
+#		sudo iptables -t mangle -A PREROUTING -o eth0 -j TTL --ttl-set 65
+		sudo echo "65" >  /proc/sys/net/ipv4/ip_default_ttl
 		zenity --info --text "Значение TTL установлено в 65"
 	;;
 
 	Выключить)
-		(iptables -t mangle -A PREROUTING -o eth0 -j TTL --ttl-set 64) | zenity --progress --pulsate --title="Установка TTL в iptables" --text="Прогресс"
-		zenity --info --text "Значение TTL установлено в $64"
+#		if ( $ttlrem > 0 ); then
+#		sudo iptables -t mangle -D POSTROUTING $ttlrem
+#		fi
+#		sudo iptables -t mangle -A PREROUTING -o eth0 -j TTL --ttl-set 64
+		sudo echo "64" >  /proc/sys/net/ipv4/ip_default_ttl
+		zenity --info --text "Значение TTL установлено в 64"
 	;;
 
 	$inst)
 		ans=$(zenity --entry --text="Введите значение TTL вручную:");
 sleep 3
-#		(iptables -t mangle -A PREROUTING -o eth0 -j TTL --ttl-set $ans) | zenity --progress --pulsate --title="Установка TTL в iptables" --text="Прогресс"
-		zenity --info --text "Значение TTL установлено в $ans"
+#		sudo iptables -t mangle -A PREROUTING -o eth0 -j TTL --ttl-set $ans
+		gksudo echo $ans >  /proc/sys/net/ipv4/ip_default_ttl
+		zenity --info --text "Значение TTL установлено в $ans "
 	;;
 
 	*)
@@ -25,7 +34,6 @@ sleep 3
 	;;
 	esac
 
-echo " Значение TTL = $ans "
+echo "Значение TTL = $ans "
 #iptables -t mangle -A PREROUTING -o eth0 -j TTL --ttl-set $ans
 exit
-
